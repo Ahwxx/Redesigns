c374ebfae7dec63b8db234784f59b995dcc723dd
Ahwx <jaap@jaapvd.nl>
2022-04-28 20:07:52 +0200

    🔵 - Add code snippets map and add rounded borders code snippet

---
+++ Snippets/Rounded Borders.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+namespace Rounded_Borders
+{
+    public partial class MainForm : Form
+    {
+        [DllImport("Gdi32.dll", EntryPoint = "CreateRoundRectRgn")]
+        private static extern IntPtr CreateRoundRectRgn
+        (
+            int nLeftRect,     // x-coordinate of upper-left corner
+            int nTopRect,      // y-coordinate of upper-left corner
+            int nRightRect,    // x-coordinate of lower-right corner
+            int nBottomRect,   // y-coordinate of lower-right corner
+            int nWidthEllipse, // width of ellipse
+            int nHeightEllipse // height of ellipse
+        );
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.FormBorderStyle = FormBorderStyle.None;
+            Region = System.Drawing.Region.FromHrgn(CreateRoundRectRgn(0, 0, Width, Height, 20, 20));
+        }
+    }
+}
